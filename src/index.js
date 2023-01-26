0db6144832b85ad327aa1f6af7e6f68a7e01cd6d
Marcelo Bastino <marcelobastino@gmail.com>
2023-01-25 22:06:22 -0300

    agregados item

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxI2V777NCqyz054VucjUpJrFruPAxaSo",
+  authDomain: "vj-bebidas.firebaseapp.com",
+  projectId: "vj-bebidas",
+  storageBucket: "vj-bebidas.appspot.com",
+  messagingSenderId: "800105997678",
+  appId: "1:800105997678:web:8bb4e747ba9fe65bff8d7f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/ItemListContainer/ItemListContainer.js
+++ src/pages/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,14 @@
 import './itemlistcontainer.css'
+import Products from '../data/Products.json'
 import { useEffect, useState } from 'react'
 import ItemCount from '../../components/ItemCount/ItemCount'
 import ItemList from '../../components/ItemList/ItemList'
+import { Form } from 'react-bootstrap'
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState ([])
 
-  const getProducts = fetch('../../../data/Products.json');
+  const getProducts = fetch('../data/Products.json');
 
   useEffect(() => {
     getProducts
