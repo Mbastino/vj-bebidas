d177f652158d079ec18d5f64b451cdcdff8ef6b9
Marcelo Bastino <marcelobastino@gmail.com>
2023-02-10 21:16:51 -0300

    terminando

--- src/pages/ItemListContainer/ItemListContainer.js
+++ src/pages/ItemListContainer/ItemListContainer.js
@@ -1,19 +1,33 @@
-import './itemlistcontainer.css'
-import Products from '../../data/Products.json'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import ItemList from '../../components/ItemList/ItemList'
-import Loader from '../../components/Loaders/Loaders.js'
+import './itemlistcontainer.css';
+import Products from '../../data/Products.json';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ItemList from '../../components/ItemList/ItemList';
+import Loader from '../../components/Loaders/Loaders.js';
+import { getFirestore, getDocs, collection} from "firebase/firestore";
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([]);
   const [filteredProduct, setFilteredProduct] = useState ([])
   const [hasProduct, setHasProduct] = useState(false);
-
   const {category} = useParams()
 
 
-  const listproduct = new Promise((resolve) =>
+  const listproduct = () =>{
+    const db = getFirestore;
+    const querySnapshot = collection(db, "products");
+    getDocs(querySnapshot).then((response) => {
+      const data = response.docs.map((doc) => {
+        return {id:doc.id, ...doc.data()}
+      });
+      setProducts(data)
+      hasProduct(!data)
+    }).catch(error=>console.log(error))
+  };
+
+
+
+/*   const listproduct = new Promise((resolve) =>
     setTimeout(() => {
       resolve(Products);
     }, 3500)
@@ -22,11 +36,11 @@ const ItemListContainer = () => {
   listproduct
     .then((data) => setProducts(data))
     .then((data) => setHasProduct(!data));
-
-useEffect (() => {
+ */
+ useEffect (() => {
   const filterProduct = products.filter((product) => product.category === category)
   setFilteredProduct(filterProduct)
-}, [category])
+}, [category]) 
 
   return (
     <>
